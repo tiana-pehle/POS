a0a17921bf23ca9a3a23923051238502dbc0dbd1
bananasplitter <tiana_pehle@yahoo.com>
2016-05-25 02:57:42 -0700

    I keep getting stuck because RegistrKeyPad wont let me open its designer up. Been using dreamincode.net as resource

---
+++ demoPOS3/KeyPad/GenericKeyPad.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace KeyPad
+{
+    partial class GenericKeyPad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(15, 4);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(64, 64);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(85, 4);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(64, 64);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(155, 4);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(64, 64);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn14
+            // 
+            this.btn14.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn14.Location = new System.Drawing.Point(225, 4);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(64, 64);
+            this.btn14.TabIndex = 3;
+            this.btn14.Text = "-";
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.btn14_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(15, 74);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(64, 64);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(85, 74);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(64, 64);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(155, 74);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(64, 64);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn13.Location = new System.Drawing.Point(225, 74);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(64, 64);
+            this.btn13.TabIndex = 7;
+            this.btn13.Text = "+";
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.btn13_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(15, 144);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(64, 64);
+            this.btn1.TabIndex = 8;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(85, 144);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(64, 64);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(155, 144);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(64, 64);
+            this.btn3.TabIndex = 10;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(225, 144);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(64, 134);
+            this.btn12.TabIndex = 11;
+            this.btn12.Text = "Enter";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(15, 214);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(134, 64);
+            this.btn11.TabIndex = 12;
+            this.btn11.Text = "00";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.Location = new System.Drawing.Point(155, 214);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(64, 64);
+            this.btn10.TabIndex = 13;
+            this.btn10.Text = "0";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // GenericKeyPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Name = "GenericKeyPad";
+            this.Size = new System.Drawing.Size(410, 376);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn10;
+    }
+}
---
+++ demoPOS3/KeyPad/GenericKeyPad.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyPad
+{
+    public partial class GenericKeyPad: UserControl
+    {
+        public GenericKeyPad()
+        {
+            InitializeComponent();
+        }
+
+        public event KeyPressEventHandler ButtonPressed;
+
+        public void RaiseButtonPressed(char WhatToSend)
+        {
+            KeyPressEventHandler handler = ButtonPressed;
+            if(handler != null)
+            {
+                handler(this, new KeyPressEventArgs(WhatToSend));
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn14_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
